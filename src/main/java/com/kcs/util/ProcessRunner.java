5c103aa8fcb7a5acc56892a63964853034bbbded
kamieniarzk <kamieniarzk@gmail.com>
2023-11-18 16:41:56 +0100

    fix(processes): fix deadlock in ProcessRunner

--- src/main/java/com/kcs/util/ProcessRunner.java
+++ src/main/java/com/kcs/util/ProcessRunner.java
@@ -1,13 +1,13 @@
 package com.kcs.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ProcessRunner {
@@ -18,8 +18,8 @@ public class ProcessRunner {
     builder.command(commandSeparated);
     var process = builder.start();
     var streamConsumer = new StreamConsumer(process.getInputStream(), process.getErrorStream());
-    var exitCode = process.waitFor();
     streamConsumer.consume();
+    var exitCode = process.waitFor();
     return new RunResult(streamConsumer.stdIn.toString(), streamConsumer.stdErr.toString(), exitCode);
   }
 
