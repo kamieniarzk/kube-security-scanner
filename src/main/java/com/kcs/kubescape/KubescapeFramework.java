a2e942bf39fb74f55608483257bc9c73ce6a9a89
kamieniarzk <kamieniarzk@gmail.com>
2024-01-08 22:43:23 +0100

    feat(kubescape): lowercase KubescapeFramework serialization

--- src/main/java/com/kcs/kubescape/KubescapeFramework.java
+++ src/main/java/com/kcs/kubescape/KubescapeFramework.java
@@ -1,19 +1,30 @@
 package com.kcs.kubescape;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum KubescapeFramework {
+  @JsonProperty("allcontrols")
   AllControls("allcontrols"),
+  @JsonProperty("armobest")
   ArmoBest("armobest"),
+  @JsonProperty("devopsbest")
   DevOpsBest("deveopsbest"),
+  @JsonProperty("mitre")
   MITRE("mitre"),
+  @JsonProperty("nsa")
   NSA("nsa"),
+  @JsonProperty("soc2")
   SOC2("soc2"),
+  @JsonProperty("cis_aks")
   cis_aks("cis-aks-t1.2.0"),
+  @JsonProperty("cis-eks")
   cis_eks("cis-eks-t1.2.0"),
+  @JsonProperty("cis-generic")
   cis_generic("cis-v1.23-t1.0.1");
+
   private final String name;
 }
